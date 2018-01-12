81f181cbb95b9b4adf6a00e8fb0134c4ef10ddd5
MulanSales <luanmsacc@gmail.com>
2018-01-12 11:09:38 -0200

    Finalizado funcionalidades da aplicação #6

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,13 @@
-angular.module('CompartilhaRodas')
-	.controller('HomeCtrl', function($scope, $window){
+angular.module('CompartilhaRodas.home', ['ngRoute', 'firebase'])
+	.config(['$routeProvider', function($routeProvider){
+		$routeProvider
+			.when('/home', {
+				templateUrl: 'views/home.html',
+				controller: 'HomeCtrl'
+			});
+	}])
 
+	.controller('HomeCtrl', function($scope, $window, $firebaseArray){
+		var ref = firebase.database().ref('Corridas');
+		$scope.data = $firebaseArray(ref);
 	});
